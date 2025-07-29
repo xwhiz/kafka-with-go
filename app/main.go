d184042b80ea7898ccd7d0c88327c37b0ebfa430
Hamza <muhammadhamzam1486@gmail.com>
2025-07-29 11:58:11 +0500

    feat: refactor code and add multiple connection availability

--- app/main.go
+++ app/main.go
@@ -22,11 +22,19 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	scanner := bufio.NewScanner(conn)
